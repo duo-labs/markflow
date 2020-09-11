edc6c22d3b2559654d39e42f301e31195fe12510
Joshua Holland <jholland@duosecurity.com>
2020-09-11 17:28:04 -0500

    Allow for horizontal lines with * and _

--- markflow/detectors/heading.py
+++ markflow/detectors/heading.py
@@ -1,11 +1,14 @@
 from typing import List
 
 from .code_block import code_block_started
+from .horizontal_line import horizontal_line_started
 from .list import list_started
 
 
 def heading_started(line: str, index: int, lines: List[str]) -> bool:
     if not line.strip():
+        return False
+    elif horizontal_line_started(line, index, lines):
         return False
     elif line.lstrip().startswith("#") and not code_block_started(line, index, lines):
         return True
--- markflow/detectors/horizontal_line.py
+++ markflow/detectors/horizontal_line.py
@@ -2,13 +2,20 @@ from typing import List
 
 from .code_block import indented_code_block_started
 
+SEPARATOR_SYMBOLS = ["*", "_", "-"]
+
 
 def horizontal_line_started(line: str, index: int, lines: List[str]) -> bool:
-    return (
-        not indented_code_block_started(line, index, lines)
-        and len(line.strip()) >= 3
-        and all(c == "-" for c in line.strip())
-    )
+    if indented_code_block_started(line, index, lines):
+        return False
+    elif len(line.strip()) < 3:
+        # Horizontal lines must be at least three characters long
+        return False
+    else:
+        for symbol in SEPARATOR_SYMBOLS:
+            if all(char == symbol for char in line.strip()):
+                return True
+        return False
 
 
 def horizontal_line_ended(line: str, index: int, lines: List[str]) -> bool:
