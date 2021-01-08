d59d3e27c8930e98596af229014d465cc8808259
Joshua Holland <jholland@duosecurity.com>
2021-01-08 17:17:29 -0600

    Add representations of thematic blocks and other support for new logging

--- markflow/formatters/thematic_break.py
+++ markflow/formatters/thematic_break.py
@@ -8,19 +8,26 @@ __all__ = ["MarkdownThematicBreak"]
 
 
 class MarkdownThematicBreak(MarkdownSection):
+    @property
+    def char(self) -> str:
+        # Assuming we were passed valid data
+        return self.lines[0].strip()[0]
+
     def append(self, line: str) -> None:
         if self.lines:
             raise RuntimeError("Thematic breaks cannot span multiple lines")
         self.lines.append(line)
 
     def reformatted(self, width: Number = 88) -> str:
-        char = self.lines[0].strip()[0]
         if isinstance(width, float):
             if width == math.inf:
-                return char * 3
+                return self.char * 3
             else:
                 raise RuntimeError(
                     f"Invalid width {repr(width)} passed. How did you manage this?"
                 )
         else:
-            return char * width
+            return self.char * width
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: char={repr(self.char)}>"
