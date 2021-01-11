60b22529dcd972a7b4ed60f919633f47177e925d
Joshua Holland <jholland@duosecurity.com>
2021-01-11 00:05:50 -0600

    Black again

--- tests/test_link_reference_definition.py
+++ tests/test_link_reference_definition.py
@@ -8,12 +8,8 @@ from .util import create_section, render
 class TestLinkReferenceDefinition:
     # TODO: Port footnotes from file 0007 to here
     def test_basic(self) -> None:
-        input_ = (
-            "   [footnote1]: http://example.com     "
-        )
-        expected = (
-            "[footnote1]: http://example.com"
-        )
+        input_ = "   [footnote1]: http://example.com     "
+        expected = "[footnote1]: http://example.com"
         footnote = create_section(MarkdownLinkReferenceDefinition, input_)
         assert footnote.reformatted() == expected
         footnote = create_section(MarkdownLinkReferenceDefinition, expected)
